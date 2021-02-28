8f873ab90f3f66bc6c7f34a868aba5b5213ec48c
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-02-27 23:24:21 -0600

    Update Inspection.java

--- Project 1/src/maintenance/model/Inspection.java
+++ Project 1/src/maintenance/model/Inspection.java
@@ -2,34 +2,34 @@ package maintenance.model;
 import java.util.Date;
 
 public class Inspection {
-	  private int inspectionNumber;
+    private int inspectionNumber;
     private Date inspectionDate;
     private String inspectorInfo;
     private String results;
     
-	  public int getInspectionNumber() {
-		  return inspectionNumber;
+	public int getInspectionNumber() {
+	    return inspectionNumber;
 	}
-	  public void setInspectionID(int inspectionNumber) {
-		  this.inspectionNumber = inspectionNumber;
+	public void setInspectionID(int inspectionNumber) {
+	    this.inspectionNumber = inspectionNumber;
 	}
   	public Date getInspectionDate() {
-		  return inspectionDate;
+	    return inspectionDate;
 	}
-	  public void setInspectionDate(Date inspectionDate) {
-		  this.inspectionDate = inspectionDate;
+	public void setInspectionDate(Date inspectionDate) {
+	    this.inspectionDate = inspectionDate;
 	}
   	public String getInspectorInfo() {
-		  return inspectorInfo;
+	    return inspectorInfo;
 	}
-	  public void setInspector(String inspectorInfo) {
-		  this.inspectorInfo = inspectorInfo;
+	public void setInspector(String inspectorInfo) {
+	    this.inspectorInfo = inspectorInfo;
 	}
-	  public String getResults() {
-		  return results;
+	public String getResults() {
+	    return results;
 	}
-	  public void setOutcome(String results) {
-		  this.results = results;
+	public void setOutcome(String results) {
+	    this.results = results;
 	}
 }
 
