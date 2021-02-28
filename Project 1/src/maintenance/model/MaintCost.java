73d76f9f14641919ed90fa161680a4a1bd745717
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-02-27 23:13:58 -0600

    Update MaintCost.java

--- Project 1/src/maintenance/model/MaintCost.java
+++ Project 1/src/maintenance/model/MaintCost.java
@@ -1,5 +1,34 @@
 package maintenance.model;
+import java.util.*;
 
 public class MaintCost {
-
-}
+    private int costNumber;
+		private float laborCosts;
+		private float equipmentCosts;
+		private float totalCosts;
+		
+		public int getCostNumber() {
+			return costNumber;
+		}
+		public void setCostID(int costNumber) {
+			this.costNumber = costNumber;
+		}
+		public float getLaborCost() {
+			return laborCosts;
+		}
+		public void setLaborCost(float laborCosts) {
+			this.laborCosts = laborCosts;
+		}
+		public float getMaterialCost() {
+			return equipmentCosts;
+		}
+		public void setMaterialCost(float equipmentCost) {
+			this.equipmentCosts = equipmentCosts;
+		}
+		public float getTotalCosts() {
+			return totalCosts;
+		}
+		public void setExtra(float totalCosts) {
+			this.totalCosts = totalCosts;
+		}
+	}
