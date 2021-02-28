16df10f097379de8e54d9e5fca8564c40f391283
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-02-27 23:16:39 -0600

    Update MaintRequest.java

--- Project 1/src/maintenance/model/MaintRequest.java
+++ Project 1/src/maintenance/model/MaintRequest.java
@@ -1,5 +1,39 @@
 package maintenance.model;
+import java.util.Date;
 
 public class MaintRequest {
+      private int requestNumber;
+	    private Date dateRequested;
+	    private String requestDescription;
+	    
+	    private Maintenance maintenance;
+	    public MaintenanceRequest() {
+	    	setMaintenance(new Maintenance());
+	    }
+		  public int getRequestID() {
+			  return requestNumber;
+		}
+		  public void setRequestID(int requestID) {
+			  this.requestNumber = requestID;
+		}
+		  public Date getDateRequested() {
+			  return dateRequested;
+		}
+		  public void setDateRequested(Date dateRequested) {
+			  this.dateRequested = dateRequested;
+		}
+		public String getrequestDescription() {
+			return requestDescription;
+		}
+	  	public void setTextDescription(String requestDescription) {
+			  this.requestDescription = requestDescription;
+		}
+	  	public Maintenance getMaintenance() {
+			  return maintenance;
+		}
+		  public void setMaintenance(Maintenance maintanence) {
+		  	this.maintenance = maintanence;
+		}
+	}
 
-}
+
