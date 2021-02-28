74a1e46df1df0349d68ac700318fc203f7369010
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-02-27 23:21:35 -0600

    Update Maintenance.java

--- Project 1/src/maintenance/model/Maintenance.java
+++ Project 1/src/maintenance/model/Maintenance.java
@@ -1,5 +1,43 @@
 package maintenance.model;
+import java.util.*;
 
-public interface Maintenance {
+public class Maintenance {
+    private int maintenanceCode;
+    private String status = "resolving";
+    private String maintenanceProblem;
+
+    private MaintenanceSchedule schedule;
+
+    public MaintenanceSchedule getSchedule() {
+        return schedule;
+    }
+    public void setSchedule(MaintenanceSchedule schedule) {
+        this.schedule = schedule;
+    }
+    public MaintenanceSchedule scheduleMaintenance(Date date) {
+    	MaintenanceSchedule s = new MaintenanceSchedule(date);
+        setSchedule(s);
+        setStatus("Maintenance has been scheduled");
+        return s;
+    }
+	  public int getMaintenanceCode() {
+		  return maintenanceCode;
+	}
+	  public void setMaintenanceID(int maintenanceCode) {
+		  this.maintenanceCode = maintenanceCode;
+	}
+  	public String getStatus() {
+		  return status;
+	}
+	  public void setStatus(String status) {
+		  this.status = status;
+	}
+	  public String getProblemDescription() {
+		  return maintenanceProblem;
+	}
+	  public void setProblemDescription(String problemDescription) {
+      this.maintenanceProblem = problemDescription;
+	}
 
 }
+
