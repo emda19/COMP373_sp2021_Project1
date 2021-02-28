24c142949ccd90473d90a4856febec81cf311cf0
Kelvin Bui <47256422+kbui123@users.noreply.github.com>
2021-02-27 23:15:13 -0600

    Update MaintSchedule.java

--- Project 1/src/maintenance/model/MaintSchedule.java
+++ Project 1/src/maintenance/model/MaintSchedule.java
@@ -1,5 +1,33 @@
 package maintenance.model;
+import java.util.Date;
 
 public class MaintSchedule {
-
+    private int scheduleNumber;
+    private Date dateScheduled;
+    private MaintenanceRequest service = new MaintenanceRequest();
+    
+    public MaintenanceSchedule(Date date) {
+    	setDateScheduled(date);
+    	setService(new MaintenanceRequest());
+    }
+	  public int getScheduleNumber() {
+		  return scheduleNumber;
+	}
+	  public void setScheduleID(int scheduleID) {
+		  this.scheduleNumber = scheduleID;
+	}
+	  public Date getDateScheduled() {
+		  return dateScheduled;
+	}
+	  public void setDateScheduled(Date dateScheduled) {
+		  this.dateScheduled = dateScheduled;
+	}
+	  public MaintenanceRequest getService() {
+		  return service;
+	}
+	  public void setService(MaintenanceRequest service) {
+		  this.service = service;
+	} 
 }
+
+
